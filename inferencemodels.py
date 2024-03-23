3c40c58d0cf1441c190e16301d93db055293cb18
b-alvinge <b.alvinge@gmail.com>
2024-03-23 09:59:57 +0100

    cleaned file and added another inference function.

--- inferencemodels.py
+++ inferencemodels.py
@@ -1,19 +1,9 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+
 import numpy as np
-
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.neural_network import MLPRegressor
-from sklearn.cross_decomposition import PLSRegression
-
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
-from math import sqrt
 
 
 from causalnlp import Autocoder
-
-from tqdm import tqdm
 
 import torch
 
@@ -31,7 +21,7 @@ df['script'] = df['script'].replace(r'_x000D_','', regex=True)
 ac = Autocoder()
 torch.cuda.empty_cache()
 
-# Function to infer genres for a given text.
+# Function to infer genres for a given set of texts.
 def infer_genres(df, input_column, multilabel):
     genre_nli_template = "The genre of this text is {}"
     genres = [
@@ -52,19 +42,25 @@ def infer_genres(df, input_column, multilabel):
     ]
     df = ac.code_custom_topics(docs=df[input_column].values, df=df, labels=genres,
                                nli_template = genre_nli_template, max_length=512, multilabel=multilabel,
-                               batch_size=12)
+                               batch_size=64)
     return df
 
-# Function to infer aesthetic qualities for a given text.
-def infer_aesthetic_qualities(df, input_column, multilabel):
-    aesthetic_nli_template = "This text has {}"
-    aesthetic_qualities = [
-        'Engaging Characters',
-        'A Compelling Plot',
-        'Universal Themes',
-        'Immersive World-building',
-        'Emotional Resonance'
-    ]
+# Function to infer genre philosophies for a given set of texts.
+def infer_genre_philosophy(df, input_column, multilabel):
+    theme_nli_template = "this text is about {}"
+    themes = ["confronting death and facing one's ghosts from the past",
+     'realising that most of success comes about through taking action',
+     "seeking immortality through finding one's destiny in life",
+     "examining one's life in order to create one's true self",
+     "making the right choices now so as to ensure a better future for all",
+    "protecting the weak and bringing the guilty to justice",
+    "how success comes when one strips away all facades and show others who one really is",
+    "when one helps others make a home, one creates a civilisation where everyone is free to live their best life",
+    "Not being enslaved by absolute power and money or one will pay the ultimate price",
+    "discovering the magic in oneself that makes life itself an art form",
+    "looking for the truth and assigning guilt in spite of the danger",
+    "learning how to love is the key to happiness"]
+
     s = (df.pop(input_column)
          .str.split(r'\s*\r\n\s*\r\n(\r\n)*', expand=True, regex=True)
          .stack()
@@ -77,48 +73,64 @@ def infer_aesthetic_qualities(df, input_column, multilabel):
     df.dropna(subset=[input_column], inplace=True)
 
 
-    df = ac.code_custom_topics(docs=df[input_column].values, df=df[['title', 'script']], labels=aesthetic_qualities,
-                               nli_template = aesthetic_nli_template, max_length=512, multilabel=multilabel,
+
+    df = ac.code_custom_topics(docs=df[input_column].values, df=df[['title', 'script', 'meta_genres', 'meta_score']], labels=themes,
+                               nli_template = theme_nli_template, max_length=512, multilabel=multilabel,
                                batch_size=32)
     return df
+
+# Function to infer aesthetic qualities for a given set of texts.
+def infer_aesthetic_qualities(df, input_column, multilabel):
+    aesthetic_nli_template = "this text is {}"
+    aesthetic_qualities = ['Terrifying',
+     'Exhilarating',
+     'Epic',
+     'Heartwarming',
+     'Enlightening',
+     'Authentic',
+     'Gritty',
+     'Inspirational',
+     'Rhythmic',
+     'Show-stopping',
+     'Competitive',
+     'Imaginative',
+     'Moody',
+     'Futuristic',
+     'Suspenseful',
+     'Hilarious',
+     'Rugged',
+     'Magical',
+     'Gripping',
+     'Intriguing',
+     'Poignant',
+     'Passionate']
+
+    s = (df.pop(input_column)
+         .str.split(r'\s*\r\n\s*\r\n(\r\n)*', expand=True, regex=True)
+         .stack()
+         .rename(input_column)
+         .reset_index(level=1, drop=True))
+
+    df = df.join(s).reset_index(drop=True)
+
+    df[input_column].replace(r'^\s*$', np.nan, inplace=True, regex=True)
+    df.dropna(subset=[input_column], inplace=True)
+
+
+
+    df = ac.code_custom_topics(docs=df[input_column].values, df=df[['title', 'script', 'meta_genres', 'meta_score']], labels=aesthetic_qualities,
+                               nli_template = aesthetic_nli_template, max_length=512, multilabel=multilabel,
+                               batch_size=64)
+    return df                                        
 
 
 #df_out = infer_genres(df, 'meta_summary', multilabel=True)
 
+df_out = infer_genre_philosophy(df, 'script', multilabel=True)
 
-df_out = infer_aesthetic_qualities(df.iloc[0:2], 'script', multilabel=True)
-
-
-
-df_out.to_hdf('./data/out/title_script_aesthetic_qualities.h5', key='df')
+#df_out = infer_aesthetic_qualities(df, 'script', multilabel=True)
 
 
-# Split the dataset into training and testing sets
-#X_train, X_test, y_train, y_test = train_test_split(df['script'], df['meta_score'], test_size=0.2, random_state=1)
 
-"""
+df_out.to_hdf('./data/out/title_script_metagenres_metascore_genre_philosophy_multilabel.h5', key='df')
 
-# Train the model
-#regressor = GradientBoostingRegressor(verbose=True)
-
-regressor = MLPRegressor(random_state=1, max_iter=1000, verbose=True, learning_rate='adaptive')
-
-#regressor = PLSRegression(n_components=100)
-
-regressor.fit(X_train_genres, y_train)
-
-# Make predictions
-predictions = regressor.predict(X_test_genres)
-
-# Evaluate the model
-rmse = sqrt(mean_squared_error(y_test, predictions))
-
-r2_score = r2_score(y_test, predictions)
-print(f'test_Root Mean Squared Error: {rmse}')
-print(f'test_R2_score: {r2_score}')
-
-# Example of making a prediction with new text
-#new_text = []
-#new_predictions = pipeline.predict(new_text)
-#print(f'Predictions: {new_predictions}')
-"""
