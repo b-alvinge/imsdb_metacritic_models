8a38e2d5270d2d7f82f4b10f94532707762e1eb0
b-alvinge <b.alvinge@gmail.com>
2024-04-05 15:10:25 +0200

    removed some code for comparing label count between dataset and model.

--- finetuningmodels.py
+++ finetuningmodels.py
@@ -26,34 +26,6 @@ dataset = Dataset.from_pandas(df)
 dataset = dataset.train_test_split(test_size=0.2)
 dataset = dataset.rename_column("meta_score", 'label')
 
-
-# Extract unique labels from both train and test sets
-unique_labels_train = set(dataset['train']['label'])
-unique_labels_test = set(dataset['test']['label'])
-
-# Combine unique labels from both sets to ensure we check all possible labels in the dataset
-all_unique_labels = unique_labels_train.union(unique_labels_test)
-
-# Define the expected range of labels for the model
-expected_labels = set(range(101))  # Since num_labels=93
-
-# Check for mismatches
-mismatched_labels = all_unique_labels - expected_labels
-if mismatched_labels:
-    print(f"Mismatched labels found in dataset: {mismatched_labels}")
-else:
-    print("No label mismatch detected. Dataset labels match model expectations.")
-
-# Additionally, check for labels expected by the model but missing in the dataset
-missing_labels = expected_labels - all_unique_labels
-if missing_labels:
-    print(f"Labels expected by the model but missing in the dataset: {missing_labels}")
-else:
-    print("All model's expected labels are present in the dataset.")
-
-
-
-
 tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
 
 
@@ -83,7 +55,7 @@ model = get_peft_model(model, lora_config)
 metric = evaluate.load("accuracy")
 
 training_args = TrainingArguments(output_dir="test_trainer", evaluation_strategy="epoch",
-                                 num_train_epochs=25,)
+                                 num_train_epochs=100,)
 
 trainer = Trainer(
     model=model,
